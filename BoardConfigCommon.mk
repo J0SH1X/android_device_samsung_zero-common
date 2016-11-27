3d609476f1065120828575d844596ac1a67e3e47
Christopher N. Hesse <raymanfx@gmail.com>
2016-11-27 20:10:39 +0100

    Enable skia FIMG2D optimizations

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -64,6 +64,8 @@ ENABLE_CPUSETS := true
 
 # FIMG2D
 BOARD_USES_SKIA_FIMGAPI := true
+BOARD_USES_NEON_BLITANTIH := true
+BOARD_USES_FIMGAPI_V4L2 := false
 
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
