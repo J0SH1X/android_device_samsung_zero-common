34066a8d38cbf6ffc4283137c4d54a12232273f1
Brandon McAnsh <brandon.mcansh@gmail.com>
2017-01-03 16:09:04 -0500

    zero: Convert to board/product fragments

---
+++ product/init.mk
@@ -0,0 +1,10 @@
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.samsungexynos7420 \
+    init.baseband.rc \
+    init.bluetooth.rc \
+    init.gps.rc \
+    init.samsungexynos7420.rc \
+    init.samsungexynos7420.usb.rc \
+    init.wifi.rc \
+    ueventd.samsungexynos7420.rc
