e67e3a07a2429acc4df3402151dc9d9ed218adc9
ルーク <lucapolesel@outlook.it>
2017-01-07 13:24:43 +0100

    Revert "use adb only"
    This reverts commit 3feae674910bea16c50bd760dfccc5b1b9ec3d43.

--- zero-common.mk
+++ zero-common.mk
@@ -238,7 +238,7 @@ PRODUCT_COPY_FILES += \
 
 # Set default USB interface
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=adb
+    persist.sys.usb.config=mtp
 
 # call Samsung LSI board support package
 $(call inherit-product, hardware/samsung_slsi-cm/exynos5/exynos5.mk)
